1dbf3ce62e08e0b8a4b725e28d9eae75b3b0d50b
Aydin Burak "Kakule" Kuscu <a.brkkuscu@gmail.com>
2023-01-21 16:36:01 +0300

    Initialize eslint and prettier

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(title, author, numOfPages, read) {
-    this.title = title;
-    this.author = author;
-    this.numOfPages = numOfPages;
-    this.read = read;
+  this.title = title;
+  this.author = author;
+  this.numOfPages = numOfPages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
