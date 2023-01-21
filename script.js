78fbd032011742eb5d933b3f01a399e563424cdc
Aydin Burak "Kakule" Kuscu <a.brkkuscu@gmail.com>
2023-01-21 15:46:56 +0300

    Initialize project

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(title, author, numOfPages, read) {
+    this.title = title;
+    this.author = author;
+    this.numOfPages = numOfPages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+
+}
